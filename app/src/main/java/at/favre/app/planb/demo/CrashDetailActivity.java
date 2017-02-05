bd70ab543be5603ccb51c621f9b020698f00c649
pfavre <patrick.favre@rise-world.com>
2017-02-05 17:31:29 +0100

    uses databinding in demo app updates demo app styling and layouts

--- app/src/main/java/at/favre/app/planb/demo/CrashDetailActivity.java
+++ app/src/main/java/at/favre/app/planb/demo/CrashDetailActivity.java
@@ -1,10 +1,12 @@
 package at.favre.app.planb.demo;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
 
+import at.favre.app.planb.demo.databinding.ActivityCrashDetailBinding;
 import at.favre.lib.planb.PlanB;
 import at.favre.lib.planb.data.CrashData;
 
@@ -13,15 +15,16 @@ public class CrashDetailActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ActivityCrashDetailBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_crash_detail);
 
-        setContentView(R.layout.activity_crash_detail);
+        setSupportActionBar((Toolbar) binding.toolbar.findViewById(R.id.toolbar));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         CrashData cd = PlanB.get(this).getCrashDataHandler().getLatest();
 
         if (cd != null) {
-            ((TextView) findViewById(R.id.tv_stacktrace)).setText(cd.fullStacktrace);
-            ((TextView) findViewById(R.id.tv_cause)).setText(cd.throwableClassName+" caused by "+cd.causeClassName+"."+cd.causeMethodName+"("+cd.causeFileName+":"+cd.causeLineNum+")");
-
+            binding.tvStacktrace.setText(cd.fullStacktrace);
+            binding.tvCause.setText(cd.throwableClassName + " caused by " + cd.causeClassName + "." + cd.causeMethodName + "(" + cd.causeFileName + ":" + cd.causeLineNum + ")");
         }
     }
 }
--- app/src/main/java/at/favre/app/planb/demo/MainActivity.java
+++ app/src/main/java/at/favre/app/planb/demo/MainActivity.java
@@ -1,9 +1,12 @@
 package at.favre.app.planb.demo;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import at.favre.app.planb.demo.databinding.ActivityMainBinding;
 import at.favre.lib.planb.util.CrashUtil;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,21 +15,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+        setSupportActionBar((Toolbar) binding.toolbar.findViewById(R.id.toolbar));
+        binding.button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 CrashUtil.crash();
             }
         });
-        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+        binding.button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 ((CrashApplication) getApplication()).crash();
             }
         });
-
-
     }
 }
